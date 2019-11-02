8475c97d00eb8f51c28f35710a3f77abd55e8337
Jaman Dedy <jeandedieuam@gmail.com>
2019-11-02 22:31:05 +0200

    change credentilas

--- demo/index.js
+++ demo/index.js
@@ -2,6 +2,8 @@ const something = 'hello';
 console.log('something', something);
 
 const user = {
-    name: 'eric',
-    pwd: 'eric2019'
-}
+  name: 'eric',
+  pwd: 'eric2019'
+};
+console.log('user.name', user.name);
+console.log('user.pwd', user.pwd);
